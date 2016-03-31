9bec875f5d06bf504f566a44fc25ff74132c3416
Caridad Zapatero Contreras <zzuga@zzuga.com>
2016-03-31 18:24:29 +0200

    Initial commit to discuss structure

---
+++ src/Options/ContactDetails.php
@@ -0,0 +1,137 @@
+<?php namespace Leean\Elements\Options;
+
+/**
+ * Class ContactDetails.
+ *
+ * @package Leean\Elements\Options
+ */
+class ContactDetails
+{
+	/**
+	 * Initialise the fields
+	 */
+	 public static function init( $location ) {
+		if ( function_exists( 'acf_add_local_field_group' ) ) {
+			acf_add_local_field_group( array(
+				'key' => 'group_56fd2972b29d5',
+				'title' => 'Contact Details',
+				'fields' => array(
+					array(
+						'key' => 'field_56fd2a48b4f1a',
+						'label' => 'Title',
+						'name' => 'title',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+						'readonly' => 0,
+						'disabled' => 0,
+					),
+					array(
+						'key' => 'field_56fd2a53b4f1b',
+						'label' => 'Logo',
+						'name' => 'logo',
+						'type' => 'image',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'id',
+						'preview_size' => 'thumbnail',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+					),
+					array(
+						'key' => 'field_56fd2a5ab4f1c',
+						'label' => 'Adress',
+						'name' => 'adress',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+						'readonly' => 0,
+						'disabled' => 0,
+					),
+					array(
+						'key' => 'field_56fd2a65b4f1d',
+						'label' => 'Phone',
+						'name' => 'phone',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+						'readonly' => 0,
+						'disabled' => 0,
+					),
+					array(
+						'key' => 'field_56fd2a69b4f1e',
+						'label' => 'Email',
+						'name' => 'email',
+						'type' => 'email',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+				),
+				'location' => $location,
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => 1,
+				'description' => '',
+			) );
+		}
+	}
+}
